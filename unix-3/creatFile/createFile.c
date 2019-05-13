08dc46fc8c4486fcfe8b6f95facee8259c22e84d
dawendiguo <dawendiguo88@live.com>
2019-05-13 13:38:42 +0800

    create zombie

--- unix-3/creatFile/createFile.c
+++ unix-3/creatFile/createFile.c
@@ -3,13 +3,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int main(int argc,char *argv[]){
-    if(argc != 2){
-    err_quit("Usage: main <fileName> ");
+int main(int argc,char *argv[1]){
+    if(argc != 3){
+    err_quit("Usage: main <fileName>");
     }
 
-    int fd;
-    if(( fd = creat(argv[1],FILE_MODE)) < 0){
-        err_sys("create error");
+    int fd ;
+    if((fd = creat(argv[1],FILE_MODE)) < 0){
+    err_sys("create error");
     }
+    if((fd = open(argv[2],O_WRONLY|O_TRUNC|O_CREAT,FILE_MODE)) < 0)
+        err_sys("open method create file error");
+    close(fd);
 }
--- unix-8/noZombie/createZombie.c
+++ unix-8/noZombie/createZombie.c
@@ -2,12 +2,13 @@
 int main(void){
         pid_t pid;
 
-        if(pid = fork() < 0)
+        if((pid = fork()) < 0)
                 err_sys("fork error");
         else if( pid == 0)
                 _exit(0);
+       else if(pid > 0){
 
-        sleep(4);
         system("ps");
-        exit(0);
+        sleep(2);
+       }
 }
